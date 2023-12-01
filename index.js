b44e29307f6c21aa942dc25087285fd5b7e6b658
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-01 15:06:48 +0900

    [refactor]db연결코드삭제

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const express = require('express')
 const app = express()
 const port = 8080
 // get the client
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-// Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
-});
+// // Create the connection pool. The pool-specific settings are the defaults
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   maxIdle: 10,
+//   idleTimeout: 60000,
+//   queueLimit: 0,
+//   enableKeepAlive: true,
+//   keepAliveInitialDelay: 0
+// });
 
-// For pool initialization, see above
-pool.getConnection(function(err, conn) {
-  // For pool initialization, see above
-  pool.query("SELECT * FROM University", function(err, rows, fields) {
-    // Connection is automatically released when query resolves
-    console.log(rows);
-  });
-  // Don't forget to release the connection when finished!
-  pool.releaseConnection(conn);
-});
+// // For pool initialization, see above
+// pool.getConnection(function(err, conn) {
+//   // For pool initialization, see above
+//   pool.query("SELECT * FROM University", function(err, rows, fields) {
+//     // Connection is automatically released when query resolves
+//     console.log(rows);
+//   });
+//   // Don't forget to release the connection when finished!
+//   pool.releaseConnection(conn);
+// });
 
 app.get('/', (req, res) => {
   res.json({
