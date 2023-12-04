30712dd38de9b58c92a17af864ec1529e327bd6c
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-04 11:27:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const port = 80
 app.get('/', (req, res) => {
   res.json({
     "hey" : "NodeJs",
-    "multibranch-pipeline":"wow!!",
+    "multibranch-pipeline":"wow!!~~",
   })
 })
 
 app.listen(port, () => {
   console.log(`Example nodejs app listening on port ${port}~~`)
-    // 명시적인 오류 발생
-    throw new Error('Intentional error for Jenkins check');
 
 })
